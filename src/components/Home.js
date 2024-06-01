5847b791bda78d8477f135a251e78d160f3ae179
EliorUzan <68156663+EliorUzan@users.noreply.github.com>
2024-06-01 16:39:30 +0300

    trial

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,16 +10,17 @@ const secondBlogPost_header = "big-companies-in-israel-semiconductor-industry"
 const thirdBlogPost_header = "placeHolder3"
 const fourthBlogPost_header = "placeHolder4"
 
-const firstBlogPost = await axios.get(`${host}/blogPosts/showPost/${firstBlogPost_header}`);
-const firstBlogPost_homePageSummary = await axios.get(`${host}/blogPosts/blogPostItems/${firstBlogPost.data.home_page_summary_HTML}`);
-console.log(firstBlogPost_homePageSummary)
+// const firstBlogPost = await axios.get(`${host}/blogPosts/showPost/${firstBlogPost_header}`);
+// const firstBlogPost_homePageSummary = await axios.get(`${host}/blogPosts/blogPostItems/${firstBlogPost.data.home_page_summary_HTML}`);
+
+// console.log(firstBlogPost_homePageSummary)
 const Home = () => {
   return (
     <div className="home-container">
       <h1>פוסטים אחרונים</h1>
       <div>
       <Link to="/initial-roles-in-the-industry" className='home-blog_post_link'><h2 className='home-blog_post_link'>תפקידי כניסה לתעשייה</h2></Link>
-      <div className="home-blog_post_summary" dangerouslySetInnerHTML={{ __html: firstBlogPost_homePageSummary.data }} />
+      {/* <div className="home-blog_post_summary" dangerouslySetInnerHTML={{ __html: firstBlogPost_homePageSummary.data }} /> */}
       {/* Add more content here */}
       </div>
     </div>
